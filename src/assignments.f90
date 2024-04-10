f59da01ff62b1dfaece93eeb810acc2efbd2059d
Philippe P. Pébaÿ <philippe.pebay@ng-analytics.com>
2024-04-10 08:03:03 -0400

    Completed refactoring to (1) add all missing calculations from paper and (2) match paper variable names

--- src/assignments.f90
+++ src/assignments.f90
@@ -4,27 +4,36 @@ program assignments
   integer :: I, ii, jj
   integer :: K, kk, ll
   integer :: M, mm
+  integer :: N, nn
+
+  ! block-to-task assignment matrices
+  logical, allocatable :: u_l(:,:)
+  integer, allocatable :: u_i(:,:)
+
+  ! block-to-rank assignment matrices
+  logical, allocatable :: phi_l(:,:)
+  integer, allocatable :: phi_i(:,:)
 
   ! task-to-rank assignment matrices
-  logical, allocatable :: xi_l(:,:)
-  logical, allocatable :: xi_t(:,:)
-  integer, allocatable :: xi_i(:,:)
+  logical, allocatable :: chi_l(:,:)
+  logical, allocatable :: chi_t(:,:)
+  integer, allocatable :: chi_i(:,:)
 
   ! comm-to-task assignment tensors
-  logical, allocatable :: sigma_l(:,:,:)
-  integer, allocatable :: sigma_i(:,:,:)
+  logical, allocatable :: w_l(:,:,:)
+  integer, allocatable :: w_i(:,:,:)
 
   ! comm-to-rank assignment tensors
-  logical, allocatable :: tau_l(:,:,:)
-  integer, allocatable :: tau_i(:,:,:)
+  logical, allocatable :: psi_l(:,:,:)
+  integer, allocatable :: psi_i(:,:,:)
 
   ! intermediate matrix
-  logical, allocatable :: xi_sigma_l(:,:)
+  logical, allocatable :: chi_w_l(:,:)
 
   ! tensor bounds
-  integer, allocatable :: tau_lb_i(:,:,:)
-  integer, allocatable :: tau_ub1_i(:,:,:)
-  integer, allocatable :: tau_ub2_i(:,:,:)
+  integer, allocatable :: psi_lb_i(:,:,:)
+  integer, allocatable :: psi_ub1_i(:,:,:)
+  integer, allocatable :: psi_ub2_i(:,:,:)
 
   ! sums in paper formulas
   integer :: sums(4)
@@ -32,66 +41,91 @@ program assignments
   ! integer to string conversion
   character(32) :: int_to_str
   print *
-  print *, "### Program assignments"
+  print *, "### Full Work Model Problem Example"
   print *
 
   ! allocate assignments
   I = 2
   K = 3
   M = 4
-  allocate(xi_l(I, K))
-  allocate(xi_t(I, K))
-  allocate(xi_i(I, K))
-  allocate(sigma_l(K, K, M))
-  allocate(sigma_i(K, K, M))
-  allocate(xi_sigma_l(I, K))
-  allocate(tau_l(I, I, M))
-  allocate(tau_i(I, I, M))
-  allocate(tau_lb_i(I, I, M))
-  allocate(tau_ub1_i(I, I, M))
-  allocate(tau_ub2_i(I, I, M))
+  N = 2
+  allocate(u_l(K, N))
+  allocate(u_i(K, N))
+  allocate(phi_l(I, N))
+  allocate(phi_i(I, N))
+  allocate(chi_l(I, K))
+  allocate(chi_t(I, K))
+  allocate(chi_i(I, K))
+  allocate(w_l(K, K, M))
+  allocate(w_i(K, K, M))
+  allocate(chi_w_l(I, K))
+  allocate(psi_l(I, I, M))
+  allocate(psi_i(I, I, M))
+  allocate(psi_lb_i(I, I, M))
+  allocate(psi_ub1_i(I, I, M))
+  allocate(psi_ub2_i(I, I, M))
 
-  ! populate and print task-rank matrices
-  xi_l(1,:) =  [.TRUE. , .TRUE. , .FALSE.]
-  xi_l(2,:) =  [.FALSE., .FALSE., .TRUE. ]
-  xi_t = transpose(xi_l)
-  xi_i = merge(1, 0, xi_l)
-  call print_logical_matrix("xi", I, K, xi_l)
-  call print_integer_matrix("xi", I, K, xi_i)
+  ! populate and print parameters
+  print *, "## Parameters:"
+  print *, "I = ", int_to_str(I)
+  print *, "K = ", int_to_str(K)
+  print *, "M = ", int_to_str(M)
+  print *, "N = ", int_to_str(N)
+  print *
+  u_l(1,:) = [.TRUE. , .FALSE.]
+  u_l(2,:) = [.TRUE. , .FALSE.]
+  u_l(3,:) = [.FALSE., .TRUE. ]
+  call print_logical_matrix("u", K, N, u_l)
+  call print_integer_matrix("u", K, N, u_i)
+  print *
+  w_l(3,1,1) = .TRUE.
+  w_l(3,2,2) = .TRUE.
+  w_l(2,3,3) = .TRUE.
+  w_l(2,1,4) = .TRUE.
+  w_i = merge(1, 0, w_l)
+  do mm = 1, M
+     call print_logical_matrix("w::"//trim(int_to_str(mm)), K, K, w_l(:,:,mm))
+  end do
+  do mm = 1, M
+     call print_integer_matrix("w::"//trim(int_to_str(mm)), K, K, w_i(:,:,mm))
+  end do
   print *
 
-  ! populate and print communication-task tensors
-  sigma_l = .FALSE.
-  sigma_l(3,1,1) = .TRUE.
-  sigma_l(3,2,2) = .TRUE.
-  sigma_l(2,3,3) = .TRUE.
-  sigma_l(2,1,4) = .TRUE.
-  sigma_i = merge(1, 0, sigma_l)
-  do mm = 1, M
-     call print_logical_matrix("sigma::"//trim(int_to_str(mm)), K, K, sigma_l(:,:,mm))
-  end do
-  do mm = 1, M
-     call print_integer_matrix("sigma::"//trim(int_to_str(mm)), K, K, sigma_i(:,:,mm))
-  end do
+  ! populate and print variables
+  print *, "## Variables:"
+  chi_l(1,:) = [.TRUE. , .TRUE. , .FALSE.]
+  chi_l(2,:) = [.FALSE., .FALSE., .TRUE. ]
+  chi_t = transpose(chi_l)
+  chi_i = merge(1, 0, chi_l)
+  call print_logical_matrix("chi", I, K, chi_l)
+  call print_integer_matrix("chi", I, K, chi_i)
+  print *
+
+  ! compute and print task-rank matrices
+  phi_l = matmul(chi_l, u_l)
+  phi_i = merge(1, 0, phi_l)
+  call print_logical_matrix("phi", I, N, phi_l)
+  call print_integer_matrix("phi", I, N, phi_i)
   print *
   
   ! compute and print communication-rank tensors
-  tau_i = merge(1, 0, tau_l)
+  psi_i = merge(1, 0, psi_l)
   do mm = 1, M
-     xi_sigma_l = matmul(xi_l, sigma_l(:,:,mm))
-     tau_l(:,:,mm) = matmul(xi_sigma_l, xi_t)
-     call print_logical_matrix("tau::"//trim(int_to_str(mm)), I, I, tau_l(:,:,mm))
+     chi_w_l = matmul(chi_l, w_l(:,:,mm))
+     psi_l(:,:,mm) = matmul(chi_w_l, chi_t)
+     call print_logical_matrix("psi::"//trim(int_to_str(mm)), I, I, psi_l(:,:,mm))
   end do
   do mm = 1, M
-     tau_i = merge(1, 0, tau_l)
-     call print_integer_matrix("tau::"//trim(int_to_str(mm)), I, I, tau_i(:,:,mm))
+     psi_i = merge(1, 0, psi_l)
+     call print_integer_matrix("psi::"//trim(int_to_str(mm)), I, I, psi_i(:,:,mm))
   end do
   print *
 
-  ! generate table
-  print *, "------------------------------------"
-  print *, "  m   j   i   l   k sklm xik xjl xxs"
-  print *, "------------------------------------"
+  ! generate integer communication tensor relations
+  print *, "# Integer communication tensor relations:"
+  print *, "------------------------------------------------------"
+  print *, "m   j   i   l   k   w  chi chiT *   +  lb  psi ub1 ub2"
+  print *, "------------------------------------------------------"
   ! iterate over tensor slices
   do mm = 1, M
      ! iterate over from rank indices
@@ -106,58 +140,61 @@ program assignments
               ! iterate over to task indices
               do kk = 1, K
                  ! update sums
-                 sums(1) = sums(1) + xi_i(ii,kk) * xi_i(jj,ll) * sigma_i(kk,ll,mm) 
-                 sums(2) = sums(2) + xi_i(ii,kk) * sigma_i(kk,ll,mm) 
-                 sums(3) = sums(3) + xi_i(jj,ll) * sigma_i(kk,ll,mm) 
-                 sums(4) = sums(4) + (xi_i(ii,kk) + xi_i(jj,ll)) * sigma_i(kk,ll,mm)
+                 sums(1) = sums(1) + chi_i(ii,kk) * chi_i(jj,ll) * w_i(kk,ll,mm) 
+                 sums(2) = sums(2) + chi_i(ii,kk) * w_i(kk,ll,mm) 
+                 sums(3) = sums(3) + chi_i(jj,ll) * w_i(kk,ll,mm) 
+                 sums(4) = sums(4) + (chi_i(ii,kk) + chi_i(jj,ll)) * w_i(kk,ll,mm)
                  
                  ! print innermost loop results
-                 print "(I4,I4,I4,I4,I4,I4,I4,I4,I4)", &
-                      & mm, jj, ii, ll, kk, sigma_i(kk,ll,mm), xi_i(ii,kk), xi_i(jj,ll), &
-                      & xi_i(ii,kk) * xi_i(jj,ll) * sigma_i(kk,ll,mm)
+                 print "(I2,I4,I4,I4,I4,I4,I4,I4,I4)", &
+                      & mm, jj, ii, ll, kk, w_i(kk,ll,mm), chi_i(ii,kk), chi_i(jj,ll), &
+                      & chi_i(ii,kk) * chi_i(jj,ll) * w_i(kk,ll,mm)
               end do ! kk
            end do ! ll
 
            ! store and print results aggregated at i,j level
-           tau_ub1_i(ii,jj,mm) = sums(2)
-           tau_ub2_i(ii,jj,mm) = sums(3)
-           tau_lb_i(ii,jj,mm) = sums(4) - 1
-           print *, "   ---------------------------------"
-           print *, " tau_ijm Sxxs Sxiks Sxjls S(x+x)s-1"
-           print "(I6,I6,I6,I6,I6)", tau_i(ii,jj,mm), sums(1), tau_lb_i(ii,jj,mm), tau_ub1_i(ii,jj,mm), tau_ub2_i(ii,jj,mm)
-           print *, "   ---------------------------------"
+           psi_ub1_i(ii,jj,mm) = sums(2)
+           psi_ub2_i(ii,jj,mm) = sums(3)
+           psi_lb_i(ii,jj,mm) = sums(4) - 1
+           print "(I38, I4, I4, I4, I4)", sums(1), &
+                & psi_lb_i(ii,jj,mm), psi_i(ii,jj,mm), &
+                & psi_ub1_i(ii,jj,mm), psi_ub2_i(ii,jj,mm)
         end do ! jj
      end do ! ii
-     print *, "------------------------------------"
+     print *, "   --------------------------------------------------"
   end do ! mm
   
   ! print tensor bounds
   do mm = 1, M
-     call print_integer_matrix("tau_lb::"//trim(int_to_str(mm)), I, I, tau_lb_i(:,:,mm))
+     call print_integer_matrix("psi_lb::"//trim(int_to_str(mm)), I, I, psi_lb_i(:,:,mm))
   end do
   print *
   do mm = 1, M
-     call print_integer_matrix("tau_ub1::"//trim(int_to_str(mm)), I, I, tau_ub1_i(:,:,mm))
+     call print_integer_matrix("psi_ub1::"//trim(int_to_str(mm)), I, I, psi_ub1_i(:,:,mm))
   end do
   print *
   do mm = 1, M
-     call print_integer_matrix("tau_ub2::"//trim(int_to_str(mm)), I, I, tau_ub2_i(:,:,mm))
+     call print_integer_matrix("psi_ub2::"//trim(int_to_str(mm)), I, I, psi_ub2_i(:,:,mm))
   end do
   print *
 
   ! terminate program
-  deallocate(tau_ub2_i)
-  deallocate(tau_ub1_i)
-  deallocate(tau_lb_i)
-  deallocate(tau_i)
-  deallocate(tau_l)
-  deallocate(xi_sigma_l)
-  deallocate(sigma_i)
-  deallocate(sigma_l)
-  deallocate(xi_i)
-  deallocate(xi_t)
-  deallocate(xi_l)
-  print *, "End program assignments ###"
+  deallocate(psi_ub2_i)
+  deallocate(psi_ub1_i)
+  deallocate(psi_lb_i)
+  deallocate(psi_i)
+  deallocate(psi_l)
+  deallocate(chi_w_l)
+  deallocate(w_i)
+  deallocate(w_l)
+  deallocate(chi_i)
+  deallocate(chi_t)
+  deallocate(chi_l)
+  deallocate(phi_i)
+  deallocate(phi_l)
+  deallocate(u_i)
+  deallocate(u_l)
+  print *, "End of example ###"
   print *
 
 end program assignments
@@ -183,7 +220,7 @@ end subroutine print_logical_matrix
 ! print integer matrix to console
 subroutine print_integer_matrix(str, n_rows, n_cols, mat)
   implicit none
-  ! read-only input variables
+ ! read-only input variables
   character(*), intent(in) :: str
   integer, intent(in) :: n_rows
   integer, intent(in) :: n_cols
